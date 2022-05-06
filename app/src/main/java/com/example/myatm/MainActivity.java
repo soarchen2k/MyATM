362c085b62aa5e480fc8215a140cc5ef47c90dc9
Chong Chen <chenchong.blog@gmail.com>
2022-05-05 23:14:19 -0400

    Windows10

--- app/src/main/java/com/example/myatm/MainActivity.java
+++ app/src/main/java/com/example/myatm/MainActivity.java
@@ -24,10 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initView();
-    }
-
-    private void initView() {
         account = new Account();
         initSpinner();
         changeBalanceView();
